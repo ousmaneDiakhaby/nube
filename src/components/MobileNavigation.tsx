ff7048f7b65e5488cfc3c9d7662cd1bf06bebb76
ousmaneDiakhaby <diakhaby.ousmane@outlook.com>
2025-02-20 17:22:30 +0100

    chore: finalizing auth logic

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -16,6 +16,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import FileUploader from "@/components/FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
   ownerId: string;
@@ -110,7 +111,7 @@ const MobileNavigation = ({
               <Button
                 type="submit"
                 className="mobile-sign-out-button "
-                onClick={() => {}}
+                onClick={async () => await signOutUser()}
               >
                 <Image
                   src="assets/icons/logout.svg"
