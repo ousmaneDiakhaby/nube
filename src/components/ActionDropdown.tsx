dab45ae3203715eba35f2cbe365368f5b63d34e1
ousmaneDiakhaby <diakhaby.ousmane@outlook.com>
2025-02-21 21:25:24 +0100

    feat: file deleting system

--- src/components/ActionDropdown.tsx
+++ src/components/ActionDropdown.tsx
@@ -22,7 +22,11 @@ import { constructDownloadUrl } from "@/lib/utils";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions";
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers,
+} from "@/lib/actions/file.actions";
 import { usePathname } from "next/navigation";
 import { FileDetails, ShareInput } from "@/components/ActionsModalContent";
 
@@ -53,7 +57,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
       rename: () =>
         renameFile({ fileId: file.$id, name, extension: file.extension, path }),
       share: () => updateFileUsers({ fileId: file.$id, emails, path }),
-      delete: () => console.log("delete"),
+      delete: () =>
+        deleteFile({
+          fileId: file.$id,
+          path,
+          bucketFileId: file.bucketFileId,
+        }),
     };
 
     success = await actions[action.value as keyof typeof actions]();
@@ -100,6 +109,12 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
               onInputChange={setEmails}
               onRemove={handleRemoveUser}
             />
+          )}
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete{" "}
+              <span className="delete-file-name">{file.name}</span>?
+            </p>
           )}
         </DialogHeader>
         {["rename", "share", "delete"].includes(value) && (
--- src/components/ActionsModalContent.tsx
+++ src/components/ActionsModalContent.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { Models } from "node-appwrite";
 import Thumbnail from "@/components/Thumbnail";
 import FormattedDateTime from "@/components/FormattedDateTime";
 import { convertFileSize, formatDateTime } from "@/lib/utils";
+import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -12,7 +12,7 @@ const ImageThumbnail = ({ file }: { file: Models.Document }) => (
     <Thumbnail type={file.type} extension={file.extension} url={file.url} />
     <div className="flex flex-col">
       <p className="subtitle-2 mb-1">{file.name}</p>
-      <FormattedDateTime date={file.createdAt} className="caption" />
+      <FormattedDateTime date={file.$createdAt} className="caption" />
     </div>
   </div>
 );
@@ -29,10 +29,10 @@ export const FileDetails = ({ file }: { file: Models.Document }) => {
     <>
       <ImageThumbnail file={file} />
       <div className="space-y-4 px-2 pt-2">
-        <DetailRow label="Format :" value={file.extension} />
-        <DetailRow label="Size :" value={convertFileSize(file.size)} />
-        <DetailRow label="Owner :" value={file.owner.fullName} />
-        <DetailRow label="Last edit :" value={formatDateTime(file.updatedAt)} />
+        <DetailRow label="Format:" value={file.extension} />
+        <DetailRow label="Size:" value={convertFileSize(file.size)} />
+        <DetailRow label="Owner:" value={file.owner.fullName} />
+        <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
       </div>
     </>
   );
@@ -50,16 +50,18 @@ export const ShareInput = ({ file, onInputChange, onRemove }: Props) => {
       <ImageThumbnail file={file} />
 
       <div className="share-wrapper">
-        <p className="subtitle-2 pl-1 text-light-100">Share with other users</p>
+        <p className="subtitle-2 pl-1 text-light-100">
+          Share file with other users
+        </p>
         <Input
           type="email"
-          placeholder="Enter mail email"
+          placeholder="Enter email address"
           onChange={(e) => onInputChange(e.target.value.trim().split(","))}
           className="share-input-field"
         />
         <div className="pt-4">
           <div className="flex justify-between">
-            <p className="subtitle-2 text-light-100">Shared with :</p>
+            <p className="subtitle-2 text-light-100">Shared with</p>
             <p className="subtitle-2 text-light-200">
               {file.users.length} users
             </p>
